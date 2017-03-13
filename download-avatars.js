fe11fd089cf4d7421c6a5ba6be58ba31f19e37ca
Bruno Farias <bruno.cabral88@hotmail.com>
2017-03-13 14:30:44 -0400

    getRepoContributors function initial implementation

--- download-avatars.js
+++ download-avatars.js
@@ -1,3 +1,15 @@
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+function getRepoContributors(repoOwner, repoName, callback) {
+	var options = {
+	  url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+	  headers: {
+	    'User-Agent': 'request'
+		}
+	};
+	request.get(options,(err,response,body) => {
+		callback(err,body);
+	});
+}
+
+getRepoContributors("jquery","jquery");
