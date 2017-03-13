062b0ee777a5c87b08cb3502045682113a206b97
Bruno Farias <bruno.cabral88@hotmail.com>
2017-03-13 16:06:15 -0400

    Implemented downloadImageByURL function

--- download-avatars.js
+++ download-avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 
 var GITHUB_USER = 'bruno.cabral88@hotmail.com';
 var GITHUB_TOKEN = '07761747cb01295b5346c5bb5aba07f5c8d35a55';
@@ -15,9 +16,28 @@ function getRepoContributors(repoOwner, repoName, callback) {
 	});
 }
 
+function downloadImageByURL(url, filePath) {
+	var options = {
+		url: url,
+		headers: {
+			'User-Agent': 'GitHub Avatar Downloader - Student Project'
+		}
+	}
+	var fileOptions = {
+		flags: 'w',
+		defaultEncoding: 'utf8',
+		fd: null,
+		mode: 0o666,
+		autoClose: true
+	}
+	if (!fs.existsSync('avatars'))
+			fs.mkdirSync('avatars');
+	request.get(options).pipe(fs.createWriteStream(filePath,fileOptions));
+}
+
 getRepoContributors("jquery","jquery",(err,contributors) => {
 	if (err) throw err;
 	contributors.forEach((contributor) => {
-		console.log(contributor.avatar_url);
+		downloadImageByURL(contributor.avatar_url,`avatars/${contributor.login}.jpg`);
 	})
 });
